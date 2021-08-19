f27a682abb8539a0e851d04074db57a04821a983
Ricky(道离) <RickyLau5016@gmail.com>
2021-08-19 13:48:48 +0800

    [bugfix] Change severity of negative stats messages from WARN to DEBUG(#60375)

--- server/src/main/java/org/elasticsearch/monitor/os/OsStats.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsStats.java
@@ -236,7 +236,7 @@ public class OsStats implements Writeable, ToXContentFragment {
                 //
                 // We intentionally check for (total == 0) rather than (total - free < 0) so as not to hide
                 // cases where (free > total) which would be a different bug.
-                logger.warn("cannot compute used swap when total swap is 0 and free swap is " + free);
+                logger.debug("cannot compute used swap when total swap is 0 and free swap is " + free);
                 return new ByteSizeValue(0);
             }
             return new ByteSizeValue(total - free);
@@ -304,7 +304,7 @@ public class OsStats implements Writeable, ToXContentFragment {
                 //
                 // We intentionally check for (total == 0) rather than (total - free < 0) so as not to hide
                 // cases where (free > total) which would be a different bug.
-                logger.warn("cannot compute used memory when total memory is 0 and free memory is " + free);
+                logger.debug("cannot compute used memory when total memory is 0 and free memory is " + free);
                 return new ByteSizeValue(0);
             }
             return new ByteSizeValue(total - free);
